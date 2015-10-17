e9e2b04faaf44d04d93ba4bfeae6646b3a66b52b
Elod Csirmaz <csirmazelodpal@gmail.com>
2015-10-17 14:52:25 +0100

    Implement a simple calculator and creating items on given days

--- main.js
+++ main.js
@@ -17,6 +17,12 @@ BP = {
       if(v.match(/^\d+$/)){ return parseInt(v); }
       alert(msg + ' is not an integer number');
       return false;
+   },
+
+   checkintlist: function(v, msg){
+      if(v.match(/^[\d,]+$/)){ return true; }
+      alert(msg + ' is not a list of integers');
+      return false;
    }
 
 };
@@ -24,9 +30,6 @@ BP = {
 
 // Initialisations
 $(function(){
-
-   // Tooltips
-   $('.bptooltip').tooltip();
 
    $('.bpnavigate').on('click', function(){
       document.urlap.akcio.value = 'list'; // TODO
@@ -36,6 +39,28 @@ $(function(){
       if(typeof(absolute) != 'undefined'){ document.urlap.viewday.value = absolute; }
       document.urlap.submit();
       return false;
+   });
+
+   $('.bpheader .bpname').on('click', function(){
+      document.urlap.akcio.value = 'newon'; // TODO
+      var $this = $(this);
+      document.urlap.iuday.value = $this.closest('.bpheader').data('uday');
+      document.urlap.submit();
+      return false;
+   });
+
+   $('.calculator').on('click', function(){
+      var s = 0;
+      var x;
+      while(true){
+         x = prompt("Sum so far: "+(s/100)+". Enter a number to add, 'c' to copy the sum, or 'a' to abort");
+         if(x=='c'){
+            $('.calculator_value').val(Math.floor(s)/100);
+            break;
+         }
+         if(x=='a'){ break; }
+         s += Math.floor(x*100+.5);
+      }
    });
 
 });
